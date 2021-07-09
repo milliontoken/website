3a1a805a4b26527435f2a41fab667907aa1a1227
Fishuke <66701659+fishuke@users.noreply.github.com>
2021-07-09 15:19:02 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePage} from "./pages/home/home.page";
-import {ContactPage} from "./pages/contact/contact.page";
-import {AboutPage} from "./pages/about/about.page";
-import {BlogPage} from "./pages/blog/blog.page";
 import {LayoutComponent} from "./design/layout/layout.component";
 
 const routes: Routes = [
