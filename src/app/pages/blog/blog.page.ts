7096fe8847b43911cc453cdc4a5e9a6a984b659b
stefaniabondoc <33728976+robostephanie@users.noreply.github.com>
2021-07-09 14:15:06 +0300

    missing page components
    added missing page components

---
+++ src/app/pages/blog/blog.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.page.html',
+  styleUrls: ['./blog.page.scss']
+})
+export class BlogPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
